7c2fb1b3681ab27f775fbf18a84164cb0c723595
takdongwan <dongwantak@gamil.com>
2020-12-20 18:00:28 +0900

    Socket Console 확인

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -16,6 +18,12 @@ const handleListening = () =>
 
 
   
-const server = app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);//서버변수를 만든 이유는 socketIO에 전달하기 위해서.
 
-const io = socketIO(server);
+const io = socketIO(server); 
+let sockets = [];
+
+io.on("connection", socket => {//이벤트에서 중요한것은 CONNECTION
+  //sockets.push(socket.id);//소켓은 Request객체 , express 위에서 보내는 http 요청같은
+  
+});
