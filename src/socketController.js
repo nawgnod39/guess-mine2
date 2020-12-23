5ec4d46b1f77aeb7327049d5ed56576f5f16dccd
takdongwan <dongwantak@gmail.com>
2020-12-23 21:14:44 +0900

    Refactoring

--- src/socketController.js
+++ src/socketController.js
@@ -3,6 +3,7 @@ const socketController = socket => {
     socket.on("setNickname", ({ nickname }) => {
       console.log(nickname);
       socket.nickname = nickname;
+      socket.broadcast.emit(events.newUser, { nickname });
     });
   };
   export default socketController;
