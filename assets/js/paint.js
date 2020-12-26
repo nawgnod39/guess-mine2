ef268bb6cd1306ce17f130088c1615d54b937b66
takdongwan <dongwantak@gmail.com>
2020-12-26 16:51:56 +0900

    game part Two

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,5 +1,6 @@
 import { getSocket } from "./sockets";
 const canvas = document.getElementById("jsCanvas");
+const controls = document.getElementById("jsControls");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const mode = document.getElementById("jsMode");
@@ -92,14 +93,7 @@ const handleCM = event => {
   event.preventDefault();
 };
 
-if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
-  canvas.addEventListener("click", handleCanvasClick);
-  canvas.addEventListener("contextmenu", handleCM);
-}
+
 Array.from(colors).forEach(color =>
   color.addEventListener("click", handleColorClick)
 );
@@ -111,3 +105,27 @@ export const handleBeganPath = ({ x, y }) => beginPath(x, y);
 export const handleStrokedPath= ({ x, y }) => strokePath(x, y);
 export const handleStrokedPath = ({ x, y, color }) => strokePath(x, y, color);
 export const handleFilled = ({ color }) => fill(color);
+export const disableCanvas = () => {
+    canvas.removeEventListener("mousemove", onMouseMove);
+    canvas.removeEventListener("mousedown", startPainting);
+    canvas.removeEventListener("mouseup", stopPainting);
+    canvas.removeEventListener("mouseleave", stopPainting);
+    canvas.removeEventListener("click", handleCanvasClick);
+  };
+  
+  export const enableCanvas = () => {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+  };
+  
+  export const hideControls = () => (controls.style.opacity = 0);
+  
+  export const showControls = () => (controls.style.opacity = 1);
+  
+  if (canvas) {
+    enableCanvas();
+    canvas.addEventListener("contextmenu", handleCM);
+  }
