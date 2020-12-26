bc87443f6e3cbe13d4a80d9788665453b046557e
takdongwan <dongwantak@gmail.com>
2020-12-26 16:58:10 +0900

    game part Three

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -125,7 +125,9 @@ export const disableCanvas = () => {
   
   export const showControls = () => (controls.style.opacity = 1);
   
+  export const resetCanvas = () => fill("#fff");
+
   if (canvas) {
-    enableCanvas();
     canvas.addEventListener("contextmenu", handleCM);
+    hideControls();
   }
